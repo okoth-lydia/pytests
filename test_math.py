70c1cab335f6455de50195878c5d7cf875118d1c
neptune <eugeneogongo@live.com>
2021-01-28 11:45:49 +0300

    show how pytest.raises handles postdefined exceptions

--- test_math.py
+++ test_math.py
@@ -7,4 +7,10 @@ def test_one_plus_two():
     a = 1
     b = 2
     c = 0
-    assert a + b == c
+    assert a + b == c # assertion introspection
+
+# handling exceptions to avoid false negatives
+
+def test_divide_by_zero():
+    num = 1/0
+
