f215d6a832a57e742a1c777bd559bf491fe78f8a
neptune <eugeneogongo@live.com>
2021-01-28 08:52:54 +0300

    show a failing test case

--- test_math.py
+++ test_math.py
@@ -1,3 +1,10 @@
 # a simple pytest function
 def test_one_plus_one():
     assert 1 + 1 == 2
+
+# a failing testcase
+def test_one_plus_two():
+    a = 1
+    b = 2
+    c = 0
+    assert a + b == c
